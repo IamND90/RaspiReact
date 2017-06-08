3f5113ed761a8f72fa110cf86a458a1f19283b7a
Andrey Paramonov <andrey.oj@gmail.com>
2017-06-08 19:58:37 +0200

    rect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+//import ReactHardware, {Led} from 'react-hardware';
 import { initSp } from './js/PortManager'
 import './App.css';
 
@@ -8,25 +9,30 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-        A4: 'unknown',
+        A4: 512,
+        command:'Text',
         sp: null,
-        dataReceived: [],
+        dataReceived: '',
 
     };
 
+    initSp();
   }
 
+  showValue(value){
+      console.log(value);
+  }
 
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Raspberry Pi</h2>
+          </div>
+          <div>
+              <input type="text" contentEditable="true" value={this.state.command} onChange={this.showValue(this.value)} />
+          </div>
       </div>
     );
   }
--- my-app/src/js/PortManager.js
+++ my-app/src/js/PortManager.js
@@ -1,32 +1,39 @@
 /**
  * Created by andreyparamonov on 07.06.17.
  */
-let SerialPort = require('serialport');
+const SerialPort = require("browser-serialport").SerialPort
 
 let sp = null ;
 let dataReceived = [];
 
-export function initSp() {
+export function initSp(callback) {
      if( sp === null ){
-         sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
-         console.log('Serial Port start!');
-         sp.on("open", () => {
-             console.log('open');
-             sp.write('pins\n');
-         });
-         sp.on('data', (data) => {
-             dataReceived.push(data);
-             console.log('data received: ' + data);
-     });
- }
+
+         try{
+             sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
+             console.log('Serial Port start!');
+             sp.on("open", () => {
+                 console.log('open');
+                 sp.write('pins\n');
+
+             });
+             sp.on('data', (data) => {
+                 dataReceived.push(data);
+                 console.log('data received: ' + data);
+                 callback(data);
+             });
+         }
+         catch(error) {
+            console.log(error);
+         }
+    }
  }
 export function sendSp(data) {
      if( sp === null ){
-     this.initSp();
+        this.initSp();
      }
-     sp.write(new Buffer(data + "\n"), function(err, results) {
-     console.log('err ' + err);
-     console.log('results ' + results);
+     sp.write(new Buffer(data + "\n"), (err, results) => {
+        console.log('err ' + err);
+        console.log('results ' + results);
      });
 }
-
